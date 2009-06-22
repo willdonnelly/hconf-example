376be4c8d6f019675c880d29b8ebb5565c6a5fab
Will Donnelly <will.donnelly@gmail.com>
2009-06-21 21:38:38 -0500

    Fancy RPN calculator can add and subtract now.

--- RPN/Main.hs
+++ RPN/Main.hs
@@ -1,22 +1,47 @@
 module RPN.Main
   ( realMain
-  , State (..)
-  , defaultState
-  , Config (..)
-  , defaultConfig
+  , State  (..), defaultState
+  , Config (..), defaultConfig
   ) where
 
-data State = State { numStack :: [Int] } deriving (Read, Show)
+import System.IO
 
-defaultState :: State
+data State = State { numStack :: [Int] } deriving (Read, Show)
 defaultState = State { numStack = [] }
 
 data Config = Config { banner :: String
-                     , prompt :: String }
-
-defaultConfig :: Config
+                     , prompt :: String
+                     , functions :: [(String, [Int] -> [Int])]
+                     }
 defaultConfig = Config { banner = "RPN Calculator\n"
-                       , prompt = "> " }
+                       , prompt = "> "
+                       , functions = [("+", addStack), ("-", subStack)]
+                       }
 
 realMain :: (State -> IO ()) -> Config -> State -> IO ()
-realMain restart config state = putStrLn "Hello, world!"
+realMain restart config state = do
+    putStrLn . banner $ config
+    realMain' . numStack $ state
+  where realMain' stack = do
+            putStr (showPrompt stack) >> hFlush stdout
+            input <- getLine
+            case input of
+                 "q" -> return ()
+                 "r" -> restart . State $ stack
+                 str -> realMain' .  pushStack config str $ stack
+        showPrompt stack = "[" ++ stackString ++ "]" ++ prompt config
+            where stackString = unwords . map show . reverse $ stack
+
+pushStack :: Config -> String -> [Int] -> [Int]
+pushStack cfg str stack =
+    case str `lookup` (functions cfg) of
+         Just fn -> fn stack
+         Nothing -> read str : stack
+
+addStack []       = []
+addStack (x:[])   = [x]
+addStack (x:y:zs) = x+y:zs
+
+subStack []       = []
+subStack (x:[])   = [x]
+subStack (x:y:zs) = y-x:zs
